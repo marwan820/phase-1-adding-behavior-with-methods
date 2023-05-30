415c489c6cb0a26a75b3736dcd57d6bb230f1204
Marwan Rahim <marwan820@Marwan>
2023-05-30 15:37:10 -0500

    complete lab

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ class Dog {
 }
 
 class Bird {
-  //...
+  //
 }
 ```
 
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Cat { 
+    constructor (name,age){
+        this.name = name 
+        this.age = age ;
+       }
+       speak() {
+            return `${this.name} says meow!`
+        }}
+
+
+class Dog {
+    constructor (name,age){
+        this.name = name 
+        this.age = age 
+    } speak() {
+        return `${this.name} says woof!`
+    }
+
+
+}
+
+class Bird {
+    constructor (name,sex){
+        this.name = name 
+        this.sex = sex
+    }
+    speak(){
+      if (this.sex === 'male'){return `It's me! ${this.name}, the parrot!`}
+      else return `${this.name} says squawk!`}}
+       
+
+let cat = new Cat("Sasha","female")
+//console.log(cat.speak())
+let dog = new Dog("Rufio","male")
+let bird1 = new Bird("Pablo", "male")
+let bird2 = new Bird("Mable", "female")
+console.log(bird2.speak())
